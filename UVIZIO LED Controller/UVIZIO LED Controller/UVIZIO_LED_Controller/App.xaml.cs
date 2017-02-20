830cbe208f4a641361f8a03a09006478fc9e6481
Brayden <btraas@gmail.com>
2017-02-20 00:06:48 -0800

    Brayden - Initial commit from modified template

---
+++ UVIZIO LED Controller/UVIZIO LED Controller/UVIZIO_LED_Controller/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace UVIZIO_LED_Controller
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new UVIZIO_LED_Controller.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
